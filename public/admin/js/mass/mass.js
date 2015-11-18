19828f1c8d3694cb9a6b99404103b057a8bf592b
elenahao <elena_haoo@163.com>
2015-11-18 18:33:58 +0800

    batch send

--- public/admin/js/mass/mass.js
+++ public/admin/js/mass/mass.js
@@ -7,22 +7,31 @@
         questTable.delegate('.gm-search', 'click', function(e) {
             e.preventDefault();
             var target = $(e.currentTarget);
-            $.ajax({
-                url: questTable.attr('action'),
-                headers: {
-                    'X-CSRF-Token': csrfKey
-                },
-                type: 'POST',
-                data: questTable.serialize(),
-                dataType: 'json',
-                success: function(res) {
-                    if (res.ret == 0) {
-                        alert(res.msg);
-                    } else {
-                        alert('(╯‵□′)╯︵┻━┻ 失败......');
-                    }
-                }
-            });
+            var msg_id = questTable.find('#msg_id').val();
+            var media_id = 111;//questTable.find('#media_id').val();
+            alert(msg_id);
+            alert(media_id);
+            if(msg_id != 'undefined' && media_id != 'undefined'){
+                alert('ok')
+                //$.ajax({
+                //    url: questTable.attr('action'),
+                //    headers: {
+                //        'X-CSRF-Token': csrfKey
+                //    },
+                //    type: 'POST',
+                //    data: questTable.serialize(),
+                //    dataType: 'json',
+                //    success: function(res) {
+                //        if (res.ret == 0) {
+                //            alert(res.msg);
+                //        } else {
+                //            alert('(╯‵□′)╯︵┻━┻ 失败......');
+                //        }
+                //    }
+                //});
+            }else{
+                alert('(╯‵□′)╯︵┻━┻ 先创建一个图文消息才能群发啊！...');
+            }
         });
     });
 })($);
